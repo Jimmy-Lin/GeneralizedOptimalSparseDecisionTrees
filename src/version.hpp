9c4382f726e565d337a8c9e5d2121c6e0f9eb029
Reto Achermann <achreto@gmail.com>
2021-09-15 20:05:11 -0700

    adding versin header
    Signed-off-by: Reto Achermann <achreto@gmail.com>

---
+++ src/version.hpp
@@ -0,0 +1,17 @@
+
+#ifndef _VERSION_
+#define _VERSION_ 1
+
+#ifndef BUILD_GIT_REV
+#define BUILD_GIT_REV "unknown"
+#endif
+
+#ifndef BUILD_HOST
+#define BUILD_HOST "unknown"
+#endif
+
+#ifndef BUILD_DATE
+#define BUILD_DATE "20210915-162422"
+#endif
+
+#endif
