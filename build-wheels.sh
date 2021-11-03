c1a3d5c999a5b566fad2a27b3eb3657be178c6ca
keyan <keyan3@berkeley.edu>
2021-11-03 03:50:58 -0400

    test installation in build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -32,7 +32,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-# for PYBIN in /opt/python/*/bin/; do
-#     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-#     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-# done
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
